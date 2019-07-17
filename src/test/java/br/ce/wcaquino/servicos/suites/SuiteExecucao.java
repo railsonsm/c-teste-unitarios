d47fc46e32d09e3000ea98ed492c6be7602487fc
Railson Silva Martins <railson170@hotmail.com>
2019-07-17 17:11:32 -0300

    Conhecimento Adquirido: Suite de testes

---
+++ src/test/java/br/ce/wcaquino/servicos/suites/SuiteExecucao.java
@@ -0,0 +1,15 @@
+package br.ce.wcaquino.servicos.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.ce.wcaquino.servicos.CalculadoraTestTdd;
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+
+//@RunWith(Suite.class)
+//@SuiteClasses({ CalculadoraTestTdd.class, CalculoValorLocacaoTest.class, LocacaoServiceTest.class })
+public class SuiteExecucao {
+
+}
